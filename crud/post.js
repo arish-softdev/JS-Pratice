65c75614e1519a7126e076467e326ee339b22507
arish-softdev <arish.softdev@gmail.com>
2025-12-29 23:43:06 +0530

    Crud in JavaScript

--- crud/post.js
+++ crud/post.js
@@ -1,19 +1,19 @@
 document.getElementById("loginForm").addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const data = {
-        username: document.getElementById("username").value,
-        password: document.getElementById("password").value
-    };
+  const data = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  };
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        headers: {
-            "Content-Type" : "application/json"
-        },
-        body: JOSN.stringify(data)
-    })
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JOSN.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err));
 });
