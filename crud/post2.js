74b38f3d0c5cdaa8d666e56e7003b12327a6a415
arish-softdev <arish.softdev@gmail.com>
2025-12-30 22:48:48 +0530

    Crud in JavaScript

--- crud/post2.js
+++ crud/post2.js
@@ -1,14 +1,13 @@
 function sendData() {
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        headers: {
-            "Content-Type":"application/json"
-        },
-        body: JSON.stringify({
-            name: document.getElementById("name").values
-        })
-    })
-
-    .then(response => response.json())
-    .then(data => console.log(data));
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: document.getElementById("name").values,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
