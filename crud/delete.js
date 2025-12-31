2dc25309c2f3bddf0dcb7559c5e07aa58a233671
arish-softdev <arish.softdev@gmail.com>
2025-12-31 20:30:18 +0530

    Crud in JavaScript

---
+++ crud/delete.js
@@ -0,0 +1,14 @@
+function deleteUser() {
+    fetch("https://jsonplaceholder.typicode.com/users/1", {
+        method: "DELETE"
+    })
+    .then(response => {
+        if (response.ok) {
+            document.getElementById("result").innerText = "User deleted succesfully";
+        }
+    })
+    .catch(error => {
+        console.log("Error:", error);
+        
+    });
+}
