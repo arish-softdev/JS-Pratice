d644727239f452a1db691057de4f5ab43d1f161f
arish-softdev <arish.softdev@gmail.com>
2026-01-02 22:48:11 +0530

    GET in JavaScript

--- apiPratice/get.js
+++ apiPratice/get.js
@@ -1,35 +1,32 @@
-const btn=document.querySelector("#btn");
-    btn.addEventListener("click",getmethod);
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", getmethod);
 
-    function getmethod() {
-        fetch("https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user",{
-            method:"GET",
-            headers:{"content-type":"application/json"}
-            
-        })
-        .then((res)=>{
-            if (res.ok) {
-                return res.json()
-            }
-            throw new Error("Failed to fetch image");
-            
-        })
-        .then((data)=>{
-            const getbox=document.querySelector(".getbox");
-            getbox.innerHTML=""
-                
-            data.forEach((item) => {
-                const img=`
+function getmethod() {
+  fetch("https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user", {
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error("Failed to fetch image");
+    })
+    .then((data) => {
+      const getbox = document.querySelector(".getbox");
+      getbox.innerHTML = "";
+
+      data.forEach((item) => {
+        const img = `
                 <div class="images">
                     <h2>${"name : " + item.name}</h2>
                     <img src="${item.avatar}" alt="${item.name}"/>
                     
-                </div> `
-                getbox.innerHTML+=img;
-            })
-        })
-        .catch((error)=>{
-            console.log(error);
-            
-        })
-    }
+                </div> `;
+        getbox.innerHTML += img;
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
